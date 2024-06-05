6c02151c30a9dcc2f1ae37a058f42e69cd847048
Chri1899 <c.b-1@outlook.de>
2024-06-06 00:16:16 +0200

    Updated Dotfiles Basics v1

---
+++ scripts/fonts.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+declare -a fonts=(
+	InconsolataGo
+	FiraMono
+	JetBrainsMono
+)
+
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+	mkdir -p "$fonts_dir"
+fi
+
+cd "$fonts_dir"
+
+for font in "${fonts[@]}"; do
+	tar_file="${font}.tar.xz"
+	curl_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${tar_file}"
+	echo "Curling Font Archive from github"
+	curl -OL "$curl_url"
+	echo "Extracting Tarball"
+	sudo tar -xf "$tar_file"
+	echo "Removing tarball"
+	rm "$tar_file"
+done
