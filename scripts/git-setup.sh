cd3d38736d046bf7ce9b5a4d8697b91ae2d8d10c
chri1899 <c.b-1@outlook.de>
2024-10-11 14:38:12 +0200

    Modified git setup

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -9,3 +9,4 @@ echo "Configuring GIT"
 git config --global user.name "chri1899"
 git config --global user.email "c.b-1@outlook.de"
 git config --global init.defaultBranch "main"
+git config --global --add --bool push.autoSetupRemote true
--- scripts/gitcreaterepo.sh
+++ scripts/gitcreaterepo.sh
@@ -1,4 +1,18 @@
 #!bin/bash
-curl -u 'Chri1899' https://api.github.com/user/repos -d "{\name\":\"$1\"}";
-git init;
-git remote add origin git@github.com:Chri1899/$1.git;
+
+repo="$1"
+
+echo "Creating a new directory"
+mkdir ./$repo
+cd $repo
+
+echo "Creating a new remote repository on github"
+curl -u Chri1899 https://api.github.com/user/repos -d "{\"name\":\"$repo\"}"
+git init
+
+echo "Add README Content" > README.md
+git add README.md
+git commit -m "New Repository"
+
+git remote add origin git@github.com:Chri1899/$repo.git
+git push -u origin main
