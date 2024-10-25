ce260dfc1dffd3a0f95ab1c64abd10b8a7d223ff
Chri1899 <c.b-1@outlook.de>
2024-10-25 17:55:33 +0200

    Added lazygit to installs

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -77,3 +77,9 @@ cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
+
+# Install LAZYGIT
+LAZYGIT_VERSION=$curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep - Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
