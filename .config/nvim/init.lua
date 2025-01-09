632888ce08945441a71b88599407e885fd7a5ae6
Chri1899 <c.b-1@outlook.de>
2025-01-09 14:32:30 +0100

    Hopefully fixed all git issues

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-require 'core.options'
-require 'core.remaps'
-
-require 'lazy-init'
-
-require 'core.autocmds'
-=======
 require("config.core")
 require("config.lazy")
->>>>>>> nvim-reconfig-again
