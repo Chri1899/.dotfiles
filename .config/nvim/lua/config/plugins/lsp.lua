5416b216acde040f08b3fad1038a786bbd577fd1
Chri1899 <c.b-1@outlook.de>
2025-01-10 11:47:39 +0100

    Fixed lsp

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -22,117 +22,62 @@ return {
 	},
 
 	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = servers,
-		})
+	  require("mason").setup()
+	  local builtin = require("telescope.builtin")
+	  -- This configures the current buffer an existing LSP attaches to
+	  -- on file open
+	  vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+		callback = function(bufnr)
+		  local opts = { noremap = true }
+		  -- LSP Specific keymaps
+		  vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, opts)
+		  vim.keymap.set("n", "<leader>gr", builtin.lsp_references, opts)
+		  vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+		  vim.keymap.set("n", "<leader>gT", vim.lsp.buf.type_definition, opts)
+		  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		local lspconfig = require("lspconfig")
-
-		local servers = {
-			bashls = true,
-			lua_ls = {
-				server_capabilities = {
-					semanticTokensProvider = vim.NIL,
-				},
-			},
-			pyright = true,
-			clangd = {
-				init_options = { clangdFileStatus = true },
-				filetypes = { "c", "cpp" },
-			},
-			cmake = true,
-			jsonls = {
-				server_capabilities = {
-					documentFormattingProvider = false,
-				},
-				settings = {
-					json = {
-						validate = { enable = true },
-					},
-				},
-			},
-		}
-
-		local servers_to_install = vim.tbl_filter(function(key)
-			local t = servers[key]
-			if type(t) == "table" then
-				return not t.manual_install
-			else
-				return t
-			end
-		end, vim.tbl_keys(servers))
-
-		require("mason").setup()
-		local ensure_installed = {
-			"clangd",
-			"cmake",
-			"lua_ls",
-			"pyright",
-			"stylua",
-		}
-
-		vim.list_extend(ensure_installed, servers_to_install)
-		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-		for name, config in ipairs(servers) do
-			if config == true then
-				config = {}
-			end
-			config = vim.tbl_deep_extend("force", {}, {
-				capabilities = capabilities,
-			}, config)
-
-			lspconfig[name].setup(config)
+		  -- Code Action
+		  vim.keymap.set("n", "<leader>br", vim.lsp.buf.rename, opts)
+		  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		  vim.keymap.set("n", "<leader>gs", builtin.lsp_document_symbols, opts)
 		end
+	  })
 
-		local disbale_semantic_tokens = {
-			lua = true,
-		}
+	  -- LSP Configuration
+	  local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(args)
-				local bufnr = args.buf
-				local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+	  -- Enable the following language servers
+	  local servers = {
+		clangd = {},
+		cmake = {},
+		pyright = {},
+		lua_ls = {
+		  settings = {
+			Lua = {
+			  completion = {
+				callSnippet = "Replace",
+			  },
+			},
+		  },
+		},
+	  }
 
-				local settings = servers[client.name]
-				if type(settings) ~= "table" then
-					settings = {}
-				end
+	  -- Ensure the defined tools are installed
+	  local ensure_installed = vim.tbl_keys(servers or {})
+	  vim.list_extend(ensure_installed, {
+		"stylua", -- Used to format Lua code
+	  })
+	  require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
-				local builtin = require("telescope.builtin")
-
-				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-				vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
-				vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-				vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-
-				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
-				vim.keymap.set("n", "<leader>wd", builtin.lsp_document_symbols, { buffer = 0 })
-
-				local filetype = vim.bo[bufnr].filetype
-				if disbale_semantic_tokens[filetype] then
-					client.server_capabilities.semanticTokensProvider = nil
-				end
-
-				-- Override server capabailities
-				if settings.server_capabilities then
-					for k, v in pairs(settings.server_capabilities) do
-						if v == vim.NIL then
-							---@diagnostic disable-next-line: cast-local-type
-							v = nil
-						end
-
-						client.server_capabilities[k] = v
-					end
-				end
-			end,
-		})
-
-		require("config.autoformat").setup()
-	end,
+	  require("mason-lspconfig").setup {
+		handlers = {
+		  function(server_name)
+			local server = servers[server_name] or {}
+			server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+			require("lspconfig")[server_name].setup(server)
+		  end,
+		},
+	  }
+	end
 }
